bc1766f2df0f4d39d37009216859da094b757cee
Nosi Chefane~ <nosichefane@gmail.com>
2022-11-25 10:38:40 -0800

    connecting to mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,35 @@
-//app
+import express from 'express';
+import DB_CONNECTION from "./config/connectDatabase";
+import * as mongoose from "mongoose";
+import { PORT } from "./config/utils";
+import { errorHandler } from "./middlewares/errorMiddleware";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+mongoose
+  .connect(DB_CONNECTION)
+  .then(() => console.log("Connected to database!"))
+  .catch((e) => console.log(e));
+
+
+  app.get('/', (req, res) => {
+    res.send("Backend Assessment API running!");
+   });
+
+
+   app.use(errorHandler)
+   app.listen(PORT, () => {
+     console.log(`App is running on http://localhost:${PORT}`, app.get("port"));
+   });
+
+
+
