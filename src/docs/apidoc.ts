04da8abaeeebe432352d7a4cd47cbe64642d99e6
Nosi Chefane~ <nosichefane@gmail.com>
2022-12-03 02:57:31 -0800

    adding api documentation

---
+++ src/docs/apidoc.ts
@@ -0,0 +1,37 @@
+const apiDocumentation = {
+    openapi: '3.0.1',
+    info: {
+      version: '1.0.0',
+      title: 'Backend Assessment API - Documentation',
+      description: 'API that will be used by Minority Africa on their proposed plaform',
+      termsOfService: 'https://minorityafrica.org/',
+      contact: {
+        name: 'Nosi Chefane',
+        email: 'nosichefane@gmail.com',
+      },
+      license: {
+        name: 'Apache 2.0',
+        url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/',
+        description: 'Local Server',
+      },
+      {
+        url: 'https://backend-assessment.onrender.com',
+        description: 'Production Server',
+      },
+    ],
+    tags: [
+      {
+        name: 'Roles',
+      },
+      {
+        name: 'Users',
+      },
+    ],
+  };
+  
+  export { apiDocumentation };
